956918a0b806be892e2946dabfbfa3b09c4dfaa7
EvgeniyBokovFIT <75373950+EvgeniyBokovFIT@users.noreply.github.com>
2021-01-04 01:29:25 +0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -13,6 +13,11 @@ void FileReader::doWork(optional<vector<string>>& Text)
 
     ifstream in;
 
+    if (Text != nullopt)
+    {
+        throw("mismatch of Input and Output");
+    }
+
     vector<string> vec;
     Text = vec;
 
@@ -22,13 +27,13 @@ void FileReader::doWork(optional<vector<string>>& Text)
     {
         throw("Can not open " + this->inName);
     }
-        
+
     while (!in.eof())
     {
         getline(in, TextBuf);
 
         Text->push_back(TextBuf);
-    }    
+    }
 
     in.close();
 }
@@ -40,6 +45,11 @@ FileWriter::FileWriter(string OutputFileName)
 
 void FileWriter::doWork(optional<vector<string>>& Text)
 {
+    if (Text == nullopt)
+    {
+        throw("mismatch of Input and Output");
+    }
+
     string str;
     ofstream out;
     out.open(this->outName);
@@ -68,6 +78,10 @@ Grep::Grep(string Word)
 
 void Grep::doWork(optional<vector<string>>& Text)
 {
+    if (Text == nullopt)
+    {
+        throw("mismatch of Input and Output");
+    }
     for (size_t i = 0; i < Text->size(); i++)
     {
         if ((*Text)[i].find(this->Word) == string::npos)
@@ -79,6 +93,10 @@ void Grep::doWork(optional<vector<string>>& Text)
 
 void Sort::doWork(optional<vector<string>>& Text)
 {
+    if (Text == nullopt)
+    {
+        throw("mismatch of Input and Output");
+    }
     sort(Text->begin(), Text->end());
 }
 
@@ -90,6 +108,10 @@ Replace::Replace(string OldWord, string NewWord)
 
 void Replace::doWork(optional<vector<string>>& Text)
 {
+    if (Text == nullopt)
+    {
+        throw("mismatch of Input and Output");
+    }
     for (size_t i = 0; i < Text->size(); i++)
     {
         size_t index = (*Text)[i].find(OldWord);
@@ -109,13 +131,17 @@ Dump::Dump(string OutputFileName)
 
 void Dump::doWork(optional<vector<string>>& Text)
 {
+    if (Text == nullopt)
+    {
+        throw("mismatch of Input and Output");
+    }
     string str;
     ofstream out;
     out.open(this->outName);
     if (!out.is_open())
     {
         throw("Can not open " + this->outName);
-    }       
+    }
 
     for (size_t i = 0; i < Text->size(); i++)
     {
@@ -150,7 +176,23 @@ shared_ptr<Worker> Parser::Parse(ifstream& Workflow)
 
     if (BlockName == "readfile")
     {
-        Workflow >> FirstArgument;
+        string check;
+        getline(Workflow, check);
+        if (check.size() == 0)
+        {
+            throw("Not enough arguments in readfile description");
+        }
+        FirstArgument = check.substr(1, check.size());
+
+        size_t SpaceIndex = 1;
+        while (check[SpaceIndex] != ' ' && SpaceIndex < check.size())
+        {
+            SpaceIndex++;
+        }
+        if (SpaceIndex < check.size())
+        {
+            throw("Too much arguments in readfile description");
+        }
 
         shared_ptr<Worker> Block = make_shared<FileReader>(FirstArgument);
 
@@ -158,7 +200,23 @@ shared_ptr<Worker> Parser::Parse(ifstream& Workflow)
     }
     else if (BlockName == "writefile")
     {
-        Workflow >> FirstArgument;
+        string check;
+        getline(Workflow, check);
+        if (check.size() == 0)
+        {
+            throw("Not enough arguments in writefile description");
+        }
+        FirstArgument = check.substr(1, check.size());
+
+        size_t SpaceIndex = 1;
+        while (check[SpaceIndex] != ' ' && SpaceIndex < check.size())
+        {
+            SpaceIndex++;
+        }
+        if (SpaceIndex < check.size())
+        {
+            throw("Too much arguments in writefile description");
+        }
 
         shared_ptr<Worker> Block = make_shared<FileWriter>(FirstArgument);
 
@@ -166,7 +224,23 @@ shared_ptr<Worker> Parser::Parse(ifstream& Workflow)
     }
     else if (BlockName == "grep")
     {
-        Workflow >> FirstArgument;
+        string check;
+        getline(Workflow, check);
+        if (check.size() == 0)
+        {
+            throw("Not enough arguments in grep description");
+        }
+        FirstArgument = check.substr(1, check.size());
+
+        size_t SpaceIndex = 1;
+        while (check[SpaceIndex] != ' ' && SpaceIndex < check.size())
+        {
+            SpaceIndex++;
+        }
+        if (SpaceIndex < check.size())
+        {
+            throw("Too much arguments in grep description");
+        }
 
         shared_ptr<Worker> Block = make_shared<Grep>(FirstArgument);
 
@@ -174,14 +248,47 @@ shared_ptr<Worker> Parser::Parse(ifstream& Workflow)
     }
     else if (BlockName == "sort")
     {
+        string check;
+        getline(Workflow, check);
+
+        if (check.size() > 0)
+        {
+            throw("Too much arguments in sort description");
+        }
+
         shared_ptr<Worker> Block = make_shared<Sort>();
 
         return Block;
     }
     else if (BlockName == "replace")
     {
-        Workflow >> FirstArgument;
-        Workflow >> SecondArgument;
+        string check;
+        getline(Workflow, check);
+        if (check.size() == 0)
+        {
+            throw("Not enough arguments in replace description");
+        }
+
+        size_t SpaceIndex = 1;
+        while (check[SpaceIndex] != ' ' && SpaceIndex < check.size())
+        {
+            SpaceIndex++;
+        }
+        if (SpaceIndex == check.size())
+        {
+            throw("Not enough arguments in replace description");
+        }
+        FirstArgument = check.substr(1, SpaceIndex - 1);
+        SecondArgument = check.substr(SpaceIndex + 1, check.size()- SpaceIndex - 1);
+        size_t SecondArgumentIndex = 0;
+        while (SecondArgument[SecondArgumentIndex] != ' ' && SecondArgumentIndex < SecondArgument.size())
+        {
+            SecondArgumentIndex++;
+        }
+        if (SecondArgumentIndex < SecondArgument.size())
+        {
+            throw("Too much arguments in replace description");
+        }
 
         shared_ptr<Worker> Block = make_shared<Replace>(FirstArgument, SecondArgument);
 
@@ -189,9 +296,25 @@ shared_ptr<Worker> Parser::Parse(ifstream& Workflow)
     }
     else if (BlockName == "dump")
     {
-        Workflow >> FirstArgument;
+        string check;
+        getline(Workflow, check);
+        if (check.size() == 0)
+        {
+            throw("Not enough arguments in dump description");
+        }
+        FirstArgument = check.substr(1, check.size());
 
-        shared_ptr<Worker> Block = make_shared<Dump>(FirstArgument);;
+        size_t SpaceIndex = 1;
+        while (check[SpaceIndex] != ' ' && SpaceIndex < check.size())
+        {
+            SpaceIndex++;
+        }
+        if (SpaceIndex < (check.size() - 1))
+        {
+            throw("Too much arguments in dump description");
+        }
+
+        shared_ptr<Worker> Block = make_shared<Dump>(FirstArgument);
 
         return Block;
     }
@@ -203,7 +326,7 @@ shared_ptr<Worker> Parser::Parse(ifstream& Workflow)
     }
 }
 
-void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, string OutputFileName, bool hasCommandLineInput, 
+void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, string OutputFileName, bool hasCommandLineInput,
     bool hasCommandLineOutput)
 {
     size_t index;
@@ -215,12 +338,12 @@ void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, stri
     ifstream Workflow;
 
     Workflow.open(WorkflowFileName);
-    
+
     if (!Workflow.is_open())
     {
         throw("Can not open " + WorkflowFileName + " file");
     }
-        
+
     string BlockName;
 
     Workflow >> BlockName;
@@ -229,13 +352,15 @@ void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, stri
     {
         throw("Haven't 'desc' in" + WorkflowFileName);
     }
-        
+
     if (Workflow.eof())
     {
         throw("EOF");
     }
-        
+
     Workflow >> BlockName;
+
+    vector<int> Ids;
 
     while (BlockName != "csed")
     {
@@ -245,10 +370,13 @@ void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, stri
         }
 
         index = stoi(BlockName);
+
+        Ids.push_back(index);
+
         if (blocks.find(index) != blocks.end())
         {
             throw("id " + to_string(index) + " already used!");
-        }        
+        }
 
         blocks[index] = Parser::Parse(Workflow);
         Workflow >> BlockName;
@@ -257,7 +385,7 @@ void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, stri
     if (Workflow.eof())
     {
         throw("You have to type a block queue after the 'csed' in the " + WorkflowFileName);
-    }      
+    }
 
     while (!Workflow.eof())
     {
@@ -279,7 +407,7 @@ void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, stri
         {
             throw("You have to type '->' between block's ids when you are making a queue");
         }
-           
+
     }
 
     optional<vector<string>> Text;
@@ -290,20 +418,24 @@ void WorkflowParser::Execute(string WorkflowFileName, string InputFileName, stri
         object.doWork(Text);
     }
 
-    for (auto i = queue.begin(); i != queue.end(); )
+    for (auto i : queue)
     {
-        blocks[*i]->doWork(Text);
-        i++;
-        if (!Text && i != queue.end())
+        bool idExists = false;
+        for (size_t j = 0; j < Ids.size(); j++)
         {
-            blocks[*i]->doWork(Text);
-            if (!Text)
+            if (i == Ids[j])
             {
-                throw("mismatch of Input and Output");
-            }           
-            i++;
-        }       
-    }   
+                idExists = true;
+                break;
+            }
+        }
+        if (idExists == false)
+        {
+            string id = to_string(i);
+            throw("id " + id + " doesn't exists in blocks description");
+        }
+        blocks[i]->doWork(Text);     
+    }
     if (hasCommandLineOutput)
     {
         FileWriter object(OutputFileName);
